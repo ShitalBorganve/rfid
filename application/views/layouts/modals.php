5f06aba24f14eacfe30cff00cd52e4cac0e94838
jpgulayan460103 <jpgulayan@gmail.com>
2018-06-25 09:20:18 +0800

    last year attended textbox

--- application/views/layouts/modals.php
+++ application/views/layouts/modals.php
@@ -277,7 +277,7 @@ if($modals_sets=="admin"){
             <div class="form-group">
               <label class="col-sm-2" for="last_year_attended">Last Year Attended:</label>
               <div class="col-sm-10">
-                <input type="number" class="form-control" name="last_year_attended" placeholder="Enter Last Year Attended">
+                <input type="text" class="form-control" name="last_year_attended" placeholder="Enter Last Year Attended">
                 <p class="help-block" id="student_last_year_attended_help-block"></p>
               </div>
               
